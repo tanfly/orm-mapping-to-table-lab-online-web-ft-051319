b6d4620ba6a2d20f7bedb35ed8b604abd0c0ba42
Stella McEwen <mcewen.stella@gmail.com>
2019-06-18 21:36:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ attr_reader :id
        CREATE TABLE IF NOT EXISTS students (
          id INTEGER PRIMARY KEY, 
          name TEXT, 
-         grade TEXT
+         grade INTEGER
          )
          SQL
      DB[:conn].execute(sql) 
